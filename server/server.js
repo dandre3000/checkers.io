ffbdbeac2d6e5ae70b5ac972deda4d4788fe834c
dandre3000 <deaundre.payne@gmail.com>
2019-09-04 22:38:27 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,15 +5,20 @@
 'use strict';
 
 (() => {
+	// dependencies
 	const express = require('express');
 	const app = express();
-	const path = require('path');
 	const server = require('http').createServer(app);
-	const socketio = require('socket.io');
-	const io = socketio(server);
-	const port = process.env.PORT || 80;
 	const fs = require('fs');
+	const path = require('path');
+	const socketio = require('socket.io');
 	
+	// modules
+	const matchMaker = require('./modules/matchmaker');
+	const login = require('./modules/login');
+	const chat = require('./modules/chat');
+	
+	const port = process.env.PORT || 80;
 	server.listen(port, () => {
 		console.log('Server listening at port %d', port);
 		fs.writeFile(__dirname + '/log.txt', 'started', (err) => {
@@ -21,7 +26,6 @@
 			console.log('The file has been saved!');
 		});
 	});
-	
 	server.on('error', (err) => {
 		console.error('Server error:', err);
 	});
@@ -31,28 +35,11 @@
 	//app.use(express.static(clientPath));
 	app.use(express.static(path.join(__dirname, '../client')));
 	console.log(`Serving static from ${clientPath}`);
-	
-	// modules
-	const matchMaker = require('./modules/matchmaker');
-	const login = require('./modules/login');
-	const chat = require('./modules/chat');
-	
-	
-	let waitingPlayer = null;
 
+	const io = socketio(server);
 	io.on('connection', (sock) => {
 		
 	});
-
-	
-	
-	// Chatroom
-	const liveChat = io.of('/chat');
-	liveChat.on('connection', sock => {
-		login(sock);
-		chat(sock);
-	});
-	
 	
 	// Matchmaking
 	const mm = io.of('/mm');
